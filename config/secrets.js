da430729cea55f9afc863a5ec0fb0ee79e887755
Joseph Graham <Joseph@Chasingthefiks.net>
2020-02-06 13:10:45 -0700

    updated sendgrid key

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECRET || "Secret-String",
-    sendgridkey: "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"
+    sendgridkey: "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"
 };
