8a87c5816f116381390290d6300c32002d235d5d
Joseph Graham <Joseph@Chasingthefiks.net>
2020-02-06 16:48:54 -0700

    Implemented env auth

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECRET || "Secret-String",
-    sendgridkey: "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"
+    sendgridkey: process.env.sendgridkey || "MyKey"
 };
