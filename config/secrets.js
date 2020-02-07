d6274b4c5d9e6b31c9a809a50b060c070e74ddac
Joseph Graham <Joseph@Chasingthefiks.net>
2020-02-07 01:06:59 -0700

    Moved email functionality to middleware component

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,4 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECRET || "Secret-String",
-    sendgridkey: process.env.sendgridkey ||
-        "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"
+    sendgridkey: process.env.sendgridkey || "Your-String"
 };
